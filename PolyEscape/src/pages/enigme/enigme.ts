5ebbfc2f0efc3855e12e626f586935580404d95a
Josué <jcr.clb@gmail.com>
2018-01-19 11:42:10 +0100

    added trucs

--- PolyEscape/src/pages/enigme/enigme.ts
+++ PolyEscape/src/pages/enigme/enigme.ts
@@ -116,5 +116,28 @@ export class EnigmePage {
     prompt.present();
   }
 
+  askHelp(){
+    let alert = this.alertCtrl.create({
+      title: 'Besoin d\'aide ?',
+      subTitle: 'Voulez-vous envoyer une demande d\'aide à vos coéquipiers ?',
+      buttons: [
+        {
+          text: 'Non',
+          role: 'cancel',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Oui',
+          handler: () => {
+            this.socket.emit('help_request', {game: this.game.name, user: this.game});
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
+
 
 }
